1ee9dc7fcdf5facc2ae18ea013123872a0aa5183
devdatta <devdattanarawade97@gmail.com>
2024-11-07 19:00:57 +0530

    first

--- main.go
+++ main.go
@@ -7,5 +7,14 @@ import (
 
 func main() {
 	name:= "Hello, World!"	
+
+	var abc int =2
 	fmt.Println(name)
+	fmt.Println(abc)
+
+	var c int=20;
+	fmt.Println(c)	
+	var d =&c;
+	fmt.Println(d)
+	fmt.Println(*d)
 }
