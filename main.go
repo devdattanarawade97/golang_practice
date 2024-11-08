cebfeb22567b75da5466f818efb99f7dbdb8b6f8
devdatta <devdattanarawade97@gmail.com>
2024-11-08 11:17:16 +0530

    first

--- main.go
+++ main.go
@@ -7,14 +7,28 @@ import (
 
 func main() {
 	name:= "Hello, World!"	
-
-	var abc int =2
+    
+	//int value 
+	var a int =2
 	fmt.Println(name)
-	fmt.Println(abc)
-
+	fmt.Println(a)
+    
+	//pointers
 	var c int=20;
 	fmt.Println(c)	
 	var d =&c;
 	fmt.Println(d)
 	fmt.Println(*d)
+    //arr value
+	var arrval [5]int=[5]int{1,2,3,4,5};
+	fmt.Println(arrval)
+
+	var mapvalue=make(map[int]string);
+	mapvalue[1]="hello";
+	mapvalue[2]="world";
+	fmt.Println("map value",mapvalue)
+
+	const i int =10
+	fmt.Println("constant value",i);
+
 }
